030d13646dbaa928664cfa1fa60357658068c183
Ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2014-05-26 14:48:57 +0200

    v1.0

--- main.lua
+++ main.lua
@@ -1,12 +1,16 @@
 dist = "null"
-state = "start"
+state = "START"
 
-red = 255
+alpha = 255
 speed = 1.5
 
 window = {}
 window.width = love.graphics.getWidth()
 window.height = love.graphics.getHeight()
+
+start = {}
+start.x = window.width / 2
+start.y = window.height / 2
 
 score = {}
 score.value = 0
@@ -15,8 +19,8 @@ score.y = window.height
 score.alpha = 255
 
 target = {}
-target.x = window.width / 2
-target.y = window.height / 2
+target.x = start.x
+target.y = start.y
 target.radius = 150
 
 function isTouched(x1,y1, x2,y2)
@@ -36,19 +40,34 @@ end
 
 function love.update(dt)
 
-	if state == "run" then
+	if state == "RUN" then
 		score.y = (score.y - 20*dt)
 		score.alpha = (score.alpha - 250*dt) 
 		target.radius = (target.radius - speed*dt)
 
 		if target.radius <= 0 then
-			state = "end"
+			state = "END_MISSED"
 		end
-	elseif state == "end" then
-		red = (red - 500*dt)
-		if red <= 0 then
-			love.event.push("quit")
+	elseif state == "END_MISSED" then
+
+		if alpha > 0 then
+			alpha = (alpha - 500*dt)
+		else
+			state = "END_DISPLAY"
+			love.graphics.setBackgroundColor(0, 0, 0)
 		end
+	elseif state == "END_DISPLAY" then
+		alpha = 0
+
+		if alpha < 255 then
+			alpha = (alpha + 500*dt)
+		else
+			alpha = 255
+		end
+
+		target.x = start.x
+		target.y = start.y
+		target.radius = 150
 	end
 end
 
@@ -56,9 +75,9 @@ function love.mousepressed(x, y, button)
 
 	if button == 'l' then
 
-		if state == "start" then
-			state = "run"
-		elseif state == "run" then
+		if state == "START" then
+			state = "RUN"
+		elseif state == "RUN" then
 
 			if isTouched(target.x, target.y, x, y) then
 				--Target touched
@@ -74,17 +93,25 @@ function love.mousepressed(x, y, button)
 				target.y = love.math.random(0, window.height)
 			else
 				--Target missed
-				state = "end"
+				state = "END_MISSED"
 			end
-		elseif state == "end" then
-
+		elseif state == "END_DISPLAY" then
+			score.value = 0
+			speed = 1.5
+			state = "START"
 		end
 	end	
-end	
+end
+
+function love.keypressed(key)
+	if key == "escape" then
+		love.event.push("quit")
+	end
+end
 
 function love.draw()
 
-	if state == "start" then
+	if state == "START" then
 		love.graphics.setColor(255, 255, 255)
 		love.graphics.circle(
 			"fill", target.x, target.y, target.radius, 100 )
@@ -92,7 +119,7 @@ function love.draw()
 		love.graphics.setColor(0, 0, 0)
 		love.graphics.print(
 			"Click here", target.x - 75, target.y - 15)
-	elseif state == "run" then
+	elseif state == "RUN" then
 
 		if score.alpha > 0 then
 			love.graphics.setColor(255, 255, 255, score.alpha)
@@ -102,16 +129,33 @@ function love.draw()
 		love.graphics.setColor(255, 255, 255)
 		love.graphics.circle(
 			"fill", target.x, target.y, target.radius, 100 )
-	elseif state == "end" then
-		love.graphics.setBackgroundColor(red, 0, 0)
+	elseif state == "END_MISSED" then
 
-		if score.alpha > 0 then
-			love.graphics.setColor(255, 255, 255, score.alpha)
+		if alpha > 0 then
+			love.graphics.setBackgroundColor(alpha, 0, 0)
+
+			love.graphics.setColor(255, 255, 255, alpha)
 			love.graphics.print(score.value, score.x, score.y)
-		end
+			love.graphics.circle(
+				"fill", target.x, target.y, target.radius, 100 )
+		end			
+		
+	elseif state == "END_DISPLAY" then
 
 		love.graphics.setColor(255, 255, 255)
 		love.graphics.circle(
 			"fill", target.x, target.y, target.radius, 100 )
+
+		love.graphics.setColor(0, 0, 0)
+		love.graphics.print(
+			"Missed", target.x - 50, target.y - 25)
+
+		if score.value < 10 then 
+			love.graphics.print(
+				score.value, target.x - 5, target.y + 10)
+		else
+			love.graphics.print(
+				score.value, target.x - 15, target.y + 10)
+		end
 	end
 end
